7b641cef649f2a0d70159d1ea0f90a69a3b47962
TejasJL <167628691+TejasJL@users.noreply.github.com>
2026-05-27 21:20:44 +0530

    Refactor Awards component for responsiveness
    Updated layout for responsive design and improved image handling.

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,32 +1,42 @@
 import React from 'react'
 
-function  Awards() {
+function Awards() {
     return ( 
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6 p-5'>
-                <img src='media/images/largestBroker.svg'/>
+                {/* Responsive Columns: Stack on mobile (col-12), split on medium+ screens (col-md-6) */}
+                <div className='col-12 col-md-6 p-3'>
+                    <img src='media/images/largestBroker.svg' className='img-fluid' alt='Largest Broker'/>
                 </div>
-                <div className='col-6 p-5 mt-3'>
-                    <h1 >Largest stock broker in India </h1>
-                    <p className='mb-5'> 2+ million Zerodha clients contribute to over 15% of all retail
-                    order volumes in India daily by trading and investing in:</p>
-                    <div className='row'><div className='col-6'>
-                        <ul>
-                        <li><p>Futures and Options </p></li>
-                        <li><p>Commodity derivatives </p></li>
-                        <li><p>Currency derivatives </p></li>
-                    </ul></div>
-                    <div className='col-6'><ul>
-                        <li><p>Stocks & IPOs </p></li>
-                        <li><p>Direct mutual funds</p></li>
-                        <li><p>Bonds and Govt. Securities </p></li>
-                    </ul></div></div>
-                    <img src='media/images/pressLogos.png' style={{width:"70%"}}/>
+                
+                <div className='col-12 col-md-6 p-3 mt-3'>
+                    <h1>Largest stock broker in India</h1>
+                    <p className='mb-5'> 2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    
+                    {/* Inner rows also need to stack on mobile */}
+                    <div className='row'>
+                        <div className='col-12 col-sm-6'>
+                            <ul>
+                                <li><p>Futures and Options</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                                <li><p>Currency derivatives</p></li>
+                            </ul>
+                        </div>
+                        <div className='col-12 col-sm-6'>
+                            <ul>
+                                <li><p>Stocks & IPOs</p></li>
+                                <li><p>Direct mutual funds</p></li>
+                                <li><p>Bonds and Govt. Securities</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    {/* Replaced fixed width with img-fluid */}
+                    <img src='media/images/pressLogos.png' className='img-fluid' alt='Press Logos'/>
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
-export default Awards ;
+export default Awards;

2f0bf91d993237cd548a1e76780b4a810a2c8f5f
TejasJL <167628691+TejasJL@users.noreply.github.com>
2026-05-27 21:19:37 +0530

    Refactor Hero component for improved layout
    Updated Hero component layout and styling.

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <div className='container p-500'>
-        <div className='row text-center mb-5'>
-           
-             <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'/>
-              <h1 className='mt-5' >Invest In Everything</h1>
-              <p>Online platform to invest in stocks,derivatives,mutual funds,and more</p>
-              <button className='btn btn-primary fs-5'  style={{width:"15%", margin:"0 auto"}}>Sign Up Now</button>
-              
-                </div> 
-                </div>
+        <div className='container py-5'> 
+            <div className='row text-center mb-5'>
+                
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5 img-fluid'/>
+                
+                <h1 className='mt-3'>Invest In Everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                
+  
+                <button className='btn btn-primary fs-5 w-100 w-md-25 d-block mx-auto'>
+                    Sign Up Now
+                </button>
+            </div> 
+        </div>
     );
 }
 
